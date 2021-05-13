48b37e4e2b2c14f62b6c9d069e02b253e86c254e
clockwerkz <clockwerkz@gmail.com>
2021-05-12 22:54:11 -0700

    Adds a simple model with hardcoded Pies and Categories

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using bethanyspieshop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ namespace bethanyspieshop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddControllersWithViews();
         }
 

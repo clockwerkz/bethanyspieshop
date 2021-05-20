86363bd0840b7872878cebeca51920ebbee263c6
Carlos <clockwerkz@gmail.com>
2021-05-19 20:02:59 -0700

    still trying to get custom tag helper to work

--- TagHelpers/EmailTagHelper.cs
+++ TagHelpers/EmailTagHelper.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
-namespace bethanyspieshop.TagHelpers
+namespace BethanysPieShop.TagHelpers
 {
     public class EmailTagHelper : TagHelper
     {
@@ -14,6 +14,7 @@ namespace bethanyspieshop.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
+
             output.Attributes.SetAttribute("href", "mailto:" + Address);
             output.Content.SetContent(Content);
         }
